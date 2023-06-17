6a94c92ec8228499d65992bbb4b7a26ef116174e
Ieleny <ielenyfilgueira@hotmail.com>
2023-06-17 19:48:10 -0300

    feature: Views

---
+++ app/src/Domain/View/LeilaoView.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Leilao da Leila</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="./../../../public/assets/css/Leilao.css" />
+    <link rel="stylesheet" type="text/css" href="./../../../public/assets/css/ImageBox.css" />
+    <link rel="stylesheet" type="text/css" href="./../../../public/assets/css/Input.css" />
+</head>
+
+<body>
+    <div class="topnav">
+        <h2 class="topnav text">
+            Leilão da Leila
+        </h2>
+    </div>
+    <div class="content">
+
+        <div class="responsive">
+            <div class="gallery">
+                <a target="_blank" href="./../../../public/assets/imagens/crash-bandicoot-playstation.jpg">
+                    <img src="./../../../public/assets/imagens/crash-bandicoot-playstation.jpg" alt="Crash bandicoot"
+                        width="600" height="400">
+                </a>
+
+                <div class="desc">ID: 4987564987546</div>
+            </div>
+        </div>
+
+        <div class="responsive">
+            <div class="gallery">
+                <a target="_blank" href="./../../../public/assets/imagens/dino-crisis-playstation.jpg">
+                    <img src="./../../../public/assets/imagens/dino-crisis-playstation.jpg" alt="Cinque Terre"
+                        width="600" height="400">
+                </a>
+
+                <div class="desc">ID: 8795498135498</div>
+            </div>
+        </div>
+
+        <div class="responsive">
+            <div class="gallery">
+                <a target="_blank" href="./../../../public/assets/imagens/spider-man-playstation.png">
+                    <img src="./../../../public/assets/imagens/spider-man-playstation.png" alt="Cinque Terre"
+                        width="600" height="400">
+                </a>
+                <div class="desc">ID: 564654654654</div>
+            </div>
+        </div>
+
+        <div class="responsive">
+            <div class="gallery">
+                <a target="_blank" href="./../../../public/assets/imagens/tomb-raider-playstation.jpg">
+                    <img src="./../../../public/assets/imagens/tomb-raider-playstation.jpg" alt="Cinque Terre"
+                        width="600" height="400">
+                </a>
+                <div class="desc">ID:45798754654</div>
+            </div>
+        </div>
+
+        <div class="clearfix"></div>
+
+        <form method="post">
+
+            <h2>
+            </h2>
+
+            <div class="bloco">
+                <input placeholder="ID do produto leiloado" name="username" id="username" class="form-control" required>
+            </div>
+
+            <div class="bloco">
+                <input placeholder="Valor do lance" name="username" id="username" class="form-control" required>
+            </div>
+
+        </form>
+    </div>
+</body>
+
+</html>
