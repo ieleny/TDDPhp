b7ef665cc7b5090d0ea1c7adbd5d2812e91848bc
Ieleny <ielenyfilgueira@hotmail.com>
2023-06-17 19:46:37 -0300

    refactor: change include name

--- app/src/index.php
+++ app/src/index.php
@@ -1,10 +1,3 @@
 <?php
 
-include 'Domain/View/Leilao/LeilaoView.php';
-
-// use Ieleny\casaApostas\Domain\Entities\Usuario as Usuario;
-
-// $usuario = new Usuario();
-// $usuario->setUsuario('Teste12334');
-
-
+include 'Domain/View/UsuarioView.php';
